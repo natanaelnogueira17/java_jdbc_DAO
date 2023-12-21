49978f708e890e5fbc3fa69441ad88f2127df5cc
natanael nogueira <natanaelnogueira019@gmail.com>
2023-12-21 10:06:53 -0300

    Department entity class

---
+++ src/aplicaton/Program.java
@@ -0,0 +1,13 @@
+package aplicaton;
+
+import model.entity.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department obj =  new Department(1,"books");
+		
+		System.err.println(obj);
+	}
+
+}
