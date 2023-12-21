caabda749901da06b6575e6231a2b6f4e877f9af
natanael nogueira <natanaelnogueira019@gmail.com>
2023-12-21 10:24:31 -0300

    seller entity class

--- src/aplicaton/Program.java
+++ src/aplicaton/Program.java
@@ -1,13 +1,18 @@
 package aplicaton;
 
+import java.util.Date;
+
 import model.entity.Department;
+import model.entity.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj =  new Department(1,"books");
+		Seller seller = new Seller(1, "bob", "email@email", new Date(), 3000.0, obj);
 		
 		System.err.println(obj);
+		System.err.println(seller);
 	}
 
 }
